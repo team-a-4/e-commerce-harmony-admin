37554fe74f84287611fa91b61cae699d12dd0cf1
sarwin_ <64829298+SarwinR@users.noreply.github.com>
2023-08-28 11:35:05 +0400

    formatting + addProduct.php fix

--- PHP/addProduct.php
+++ PHP/addProduct.php
@@ -2,29 +2,21 @@
 require("Helper/dbConnect.php");
 
 if(isset($_POST['button'])){
-
     $productName = $_POST['productName'];
     $brandName = $_POST['brandName'];
     $category = $_POST['category'];
     $description = $_POST['description'];
     
-
-    $image = $_FILES['thumbnail']['name'];  //name of the image 
-    $imageTemp = $_FILES['thumbnail']['tmp_name'];  // name of file uploaded on server.
-    $imageFolder = 'uploadedimages/'.$image;
-
-
-    $product = mysqli_query($con, "SELECT product_name FROM `products` WHERE product_name = '$productName'") or die('query failed');
+    $image = $_FILES['thumbnail']['name'];
+    $product = mysqli_query($con, "SELECT product_name FROM `products` WHERE product_name = '$productName' AND product_brand = '$brandName'");
 
     if(mysqli_num_rows($product) == 0){
         $addProduct = mysqli_query($con, "INSERT INTO `products` (category, product_name, product_desc, product_brand, product_image) VALUES('$category', '$productName', '$description', '$brandName', '$image')") or die('query failed');
-        move_uploaded_file($imageTemp, $imageFolder);  //file is stored in uploadedimages folder.
         echo '<script>alert("Product added successfully");</script>';
     }
     else{
         echo '<b style="color: red;">Product already exists</b>';
+    }
 }
-}
-
 ?>
 
